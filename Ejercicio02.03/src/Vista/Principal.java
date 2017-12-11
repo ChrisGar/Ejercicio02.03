006d23ca37cb8afa301b3930b2ddf2c9a04faf76
USUARIO <USUARIO@DESKTOP-CEEAHLH>
2017-12-11 18:49:48 -0500

    creado la principal

---
+++ Ejercicio02.03/src/Vista/Principal.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vista;
+
+import Controlador.GestionDato;
+import Modelo.Cargo;
+import Modelo.Pelicula;
+import Modelo.Persona;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Principal {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        List<Persona> pL = new ArrayList<Persona>();
+        List<Pelicula> peL = new ArrayList<Pelicula>();
+        List<Cargo> cL = new ArrayList<Cargo>();
+       
+        
+        GestionDato gD = new GestionDato(pL, peL,cL);
+        VentanaPrincipal v = new VentanaPrincipal("Archivos Binarios",gD);
+        v.setVisible(true);
+    }
+}
